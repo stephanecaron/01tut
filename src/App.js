d48686e83b85f5632f315f7048bada47e602a25e
stephanecaron <stephane.caron@groupesavoie.com>
2023-01-06 14:52:43 -0400

    adding backend json stuff

--- src/App.js
+++ src/App.js
@@ -8,33 +8,28 @@ import { useEffect, useState } from 'react';
 
 
   function App() {
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
     const [newItem, setNewItem] = useState('')
     const [search, setSearch] = useState('')
 
-    useEffect(() => {
+    useEffect(()=> {
       localStorage.setItem('shoppinglist', JSON.stringify(items));
-    })
-
-    const setAndSaveItems = (newItems) => {
-      setItems(newItems);
-      localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-    }
+    }, [items])
 
       const addItem = (item) => {
         const id = items.length ? items[items.length -1].id + 1 : 1;
         const myNewItem = {id, checked:false, item };
         const listItems = [...items, myNewItem];
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
       const handleCheck = (id) => {
         const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
 
       const handleDelete = (id) => {
         const listItems = items.filter((item) => item.id !== id);
-        setAndSaveItems(listItems);
+        setItems(listItems);
       }
   
       const handleSubmit = (e) => {
