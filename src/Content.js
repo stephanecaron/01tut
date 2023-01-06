84c73a1b6cbc8a69bc5163bfaad0f8f0488ab9cf
stephanecaron <stephane.caron@groupesavoie.com>
2023-01-06 14:16:03 -0400

    redoing

---
+++ src/Content.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import itemList from './ItemList'
+
+const Content = ({items, handleCheck, handleDelete}) => {
+ 
+
+
+  return (
+ <main>
+    {items.length ? ( 
+   <itemList 
+    items={items}
+    handleCheck={handleCheck}
+    handleDelete={handleDelete}/>
+    ) : (
+        <p style={{ marginTop: '2rem'}}>Your list is empty.</p>
+    )}
+</main>
+  )
+}
+
+export default Content
